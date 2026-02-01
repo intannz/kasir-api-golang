d2f497d070ccc8bba73aa74e833fb00ebd86c43c
Intan Maharani <carimzar1943@gmail.com>
2026-02-01 17:37:34 +0700

    update config

--- util/config.go
+++ util/config.go
@@ -15,18 +15,13 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
 
-	// baca environment variables
 	viper.AutomaticEnv()
 
 	err = viper.ReadInConfig()
 	if err != nil {
-		// 2. INI KUNCINYA:
-		// Cek jenis errornya. Kalau errornya cuma "File Gak Ketemu", ABAIKAN.
-		// Jangan langsung return error.
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// File gak ada? Gpp, lanjut aja. Kita kan punya environment variable.
+			err = nil
 		} else {
-			// Kalau errornya lain (misal syntax salah), baru lapor error.
 			return
 		}
 	}
